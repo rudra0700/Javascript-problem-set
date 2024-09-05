7cd2aeb82394b0c83aafe37926aca630abaae65c
rudra0700 <baruarudra7@gmail.com>
2024-09-05 10:52:30 +0600

    js array task added

--- exercise3.js
+++ exercise3.js
@@ -48,3 +48,78 @@
   
     // console.log(toString.call(str));
   }
+  {
+    // Write a JavaScript code to reverse the array colors without using the reverse method.
+  
+    const colors = ["red", "blue", "green", "yellow", "orange"];
+    let reverseArr = [];
+    for (const color of colors) {
+      reverseArr.unshift(color);
+    }
+    // console.log(reverseArr);
+  }
+  
+  {
+    // Write a JavaScript code to get the even numbers from an array using any looping technique.
+  
+    const numbers = [12, 98, 5, 41, 23, 78, 46];
+  
+    let i = 0;
+    let newArr = [];
+  
+    while (i < numbers.length) {
+      if (numbers[i] % 2 === 0) {
+        newArr.push(numbers[i]);
+      }
+      i++;
+    }
+    // console.log(newArr);
+  }
+  
+  {
+    // Use a for...of loop to concatenate all the elements of an array into a single string.
+  
+    var numbers = ["Tom", "Tim", "Tin", "Tik"];
+    let result = "";
+    for (let i = 0; i < numbers.length; i++) {
+      result += numbers[i];
+    }
+    // console.log(result);
+  }
+  
+  {
+    const array = ["Hello", "world", "!"];
+    const result = array.join("");
+    // console.log(result); // Output: "Helloworld!"
+  }
+  
+  {
+    const array = ["Hello", "world", "!"];
+    const result = array.reduce((acc, current) => acc + current, "");
+    // console.log(result); // Output: "Helloworld!"
+  }
+  
+  {
+    const array = ["Hello", "world", "!"];
+    const result = array.toString().replace(/,/g, "");
+    // console.log(result); // Output: "Helloworld!"
+  }
+  
+  {
+    // Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+  
+    const statement = "I am a hard working person";
+  
+    let result = statement.split(" ").reverse().join(" ");
+    // console.log(result);
+  }
+  
+  {
+    const statement = "I am a hard working person";
+    let result = "";
+    const words = statement.split(" ");
+    for (let i = words.length - 1; i >= 0; i--) {
+      result += words[i] + " ";
+    }
+    console.log(result.trim());
+  }
